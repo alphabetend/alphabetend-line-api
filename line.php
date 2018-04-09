dbf14c0d14684cebebd57c9cedbb6f9c9d8ca0e0
alphabetend <38230351+alphabetend@users.noreply.github.com>
2018-04-10 06:14:46 +0700

    Update line.php

--- line.php
+++ line.php
@@ -15,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Uebe18e4ab6acfe97f72bcc9374a74977',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
